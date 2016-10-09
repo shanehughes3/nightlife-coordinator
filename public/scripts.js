ef124eebb42dfea704d21cd99dced8fbba924d63
Shane Hughes <shanehughes314@gmail.com>
2016-10-08 20:38:33 -0500

    rough results layout

--- public/scripts.js
+++ public/scripts.js
@@ -29,11 +29,25 @@ function displayResults(results) {
     var container = $("results");
     
     for (var i = 0; i < 15; i++) {
-	var div = document.createElement("div");
-	var text = document.createTextNode(bars[i].name);
-	div.appendChild(text);
+	var div = createResultElement(bars[i]);
 	container.appendChild(div);
     }
+}
+
+function createResultElement(result) {
+    var div = document.createElement("div");
+    div.setAttribute("class", "result");
+    div.innerHTML = '<a href="' + result.url + '">' +
+	'<div class="result-thumbnail">' +
+	'<img src="' + result.image_url + '"></div>' +
+	'<div class="result-details">' +
+	'<div class="result-name">' + result.name + '</div>' +
+	'<div class="result-rating">' +
+	'<img src="' + result.rating_img_url_small + '"></div>' +
+	'<div class="result-text">' + result.snippet_text + '</div>' +
+	'</div>' +
+	'</a>';
+    return div;
 }
 
 function displayError(err) {
